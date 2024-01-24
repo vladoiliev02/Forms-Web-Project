b4aeb1af25f4a6bc875315d02248d858c77d0248
vladoiliev02 <79850594+vladoiliev02@users.noreply.github.com>
2024-01-24 16:09:41 +0200

    Refactor forms.php

--- php/forms.php
+++ php/forms.php
@@ -2,18 +2,21 @@
 
 require_once("./db.php");
 
-class Form {
+class Form
+{
     public $id;
     public $title;
     public $userId;
-    public function __construct($id, $title, $userId) {
+    public function __construct($id, $title, $userId)
+    {
         $this->id = $id;
         $this->title = $title;
         $this->userId = $userId;
     }
 }
 
-function getForms($userId) {
+function getForms($userId)
+{
     $query = single_query('
         select id, title, user_id
         from form f
@@ -22,20 +25,34 @@ function getForms($userId) {
     );
 
     $forms = [];
-    foreach($query->fetchAll() as $row) {
+    foreach ($query->fetchAll() as $row) {
         array_push($forms, new Form($row['id'], $row['title'], $row['user_id']));
     }
 
     return $forms;
 }
 
-function handleGetRequest() {
-    if ($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET['userId'])) {
-        $userId = $_GET['userId'];
-        $forms = getForms($userId);
-        header('Content-Type: application/json');
-        echo json_encode($forms);
+function handleRequest()
+{
+    switch ($_SERVER['REQUEST_METHOD']) {
+        case 'GET':
+            if (isset($_GET['userId'])) {
+                $userId = $_GET['userId'];
+                handleGetRequest($userId);
+                break;
+            }
+            break;
+        default:
+            header('Location /forms/views/404.php');
+            break;
     }
 }
 
-handleGetRequest();
+function handleGetRequest($userId)
+{
+    $forms = getForms($userId);
+    header('Content-Type: application/json');
+    echo json_encode($forms);
+}
+
+handleRequest();
